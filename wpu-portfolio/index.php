127eff0cf13135b18f34ecba395bafc7ed7bfbd4
Yukiii010 <ilhamsuryaramadhan2020@gmail.com>
2025-05-27 21:23:44 +0700

    Video 6 Menambahka dan menyesuaikan kolom profil ig untuk memperlihatkan profile, nama, follower dan postingan dari instagram melalui API dari Meta

--- wpu-portfolio/index.php
+++ wpu-portfolio/index.php
@@ -24,7 +24,53 @@ $urlLatestVideo = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyAcbjrr
 $result = get_Curl($urlLatestVideo);
 $latestVideoId = $result['items'][0]['id']['videoId'];
 
+//instagram API
+$clientID = "17841410237743575";
+$accessToken = "IGACKwYcoXWIBBZAFB5N3JOX0szV1RGVzFUeEdHb2VzMmJaRFNPSVU3YkdSVWgtMFl3QzI1VFFIaUtlbEdpYnNNdmRwTU41eEYzakwxNzd4QkpCbk51Y0VUeHdOZAlV6eHA3X1JobjRLSTZAOTzBaYkVLbTBhZA0VyT2dEWjY4UFJpNAZDZD";
+
+$result2 = get_Curl("https://graph.instagram.com/v22.0/me?fields=user_id,username,profile_picture_url,followers_count&access_token=IGACKwYcoXWIBBZAFB5N3JOX0szV1RGVzFUeEdHb2VzMmJaRFNPSVU3YkdSVWgtMFl3QzI1VFFIaUtlbEdpYnNNdmRwTU41eEYzakwxNzd4QkpCbk51Y0VUeHdOZAlV6eHA3X1JobjRLSTZAOTzBaYkVLbTBhZA0VyT2dEWjY4UFJpNAZDZD");
+
+$usernameIG = $result2['username'];
+$profilePictureIG = $result2['profile_picture_url'];
+$followersIG = $result2['followers_count'];
+
+$clientID = "17841410237743575";
+$accessToken = "IGACKwYcoXWIBBZAFB5N3JOX0szV1RGVzFUeEdHb2VzMmJaRFNPSVU3YkdSVWgtMFl3QzI1VFFIaUtlbEdpYnNNdmRwTU41eEYzakwxNzd4QkpCbk51Y0VUeHdOZAlV6eHA3X1JobjRLSTZAOTzBaYkVLbTBhZA0VyT2dEWjY4UFJpNAZDZD";
+
+// Ambil data profil IG
+$result2 = get_Curl("https://graph.instagram.com/v22.0/me?fields=user_id,username,profile_picture_url,followers_count&access_token={$accessToken}");
+
+$usernameIG = $result2['username'] ?? '';
+$profilePictureIG = $result2['profile_picture_url'] ?? '';
+$followersIG = $result2['followers_count'] ?? 0;
+
+// Ambil list media terbaru (maks 5)
+$mediaList = get_Curl("https://graph.instagram.com/me/media?fields=id,media_url,media_type&access_token={$accessToken}");
+
+$mediaItems = [];
+if (isset($mediaList['data'])) {
+    $count = 0;
+    foreach ($mediaList['data'] as $media) {
+        // Jika album, ambil media anak pertama sebagai thumbnail
+        if ($media['media_type'] == 'CAROUSEL_ALBUM') {
+            $children = get_Curl("https://graph.instagram.com/{$media['id']}/children?fields=id,media_type,media_url&access_token={$accessToken}");
+            if (isset($children['data']) && count($children['data']) > 0) {
+                $media['media_url'] = $children['data'][0]['media_url'] ?? '';
+                $media['children'] = $children['data'];
+            }
+        }
+        // Jika video, update media_url ke video URL
+        if ($media['media_type'] == 'VIDEO') {
+            $mediaDetail = get_Curl("https://graph.instagram.com/{$media['id']}?fields=media_url&access_token={$accessToken}");
+            $media['media_url'] = $mediaDetail['media_url'] ?? $media['media_url'];
+        }
+        $mediaItems[] = $media;
+        $count++;
+        if ($count >= 8) break;
+    }
+}
 ?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -97,6 +143,8 @@ $latestVideoId = $result['items'][0]['id']['videoId'];
 
 
     <!--youtube dan instagram-->
+
+    <!--youtube-->
     <section class="social bg-light" id="social">
       <div class="container">
         <div class="row pt-4 mb-4">
@@ -124,31 +172,61 @@ $latestVideoId = $result['items'][0]['id']['videoId'];
               </div>
             </div>
           </div>
+
+          <!--instagarm-->
           <div class="col-md-5">
             <div class="row">
               <div class="col-md-4">
-                <img src="img/profile1.png" width="500" class="rounded-circle img-thumbnail">
+                 <img src="<?= $profilePictureIG; ?>" width="500" class="rounded-circle img-thumbnail">
               </div>
               <div class="col-md-8">
-                <h5>@Yukiii.xe</h5>
-                <p>210 followers</p>
+                <h5><?= $usernameIG ?></h5>
+                <p><?= $followersIG ?> Followers.</p>
+                <a href="https://www.instagram.com/yuukii.xe?igsh=bmx5dGZ3ZXo2MWx4" target="_blank" rel="noopener noreferrer">
+                  <div style="
+                    background-color: #E1306C;
+                    color: white;
+                    padding: 5px 13px;
+                    border-radius: 6px;
+                    display: inline-block;
+                    font-weight: 600;
+                    font-family: sans-serif;
+                    font-size: 13px;
+                    text-decoration: none;
+                    transition: all 0.3s ease;
+                  "
+                  onmouseover="this.style.backgroundColor='#C1275A'"
+                  onmouseout="this.style.backgroundColor='#E1306C'">
+                    Follow
+                  </div>
+                </a>
               </div>
-            </div>
+            </div> 
 
             <div class="row mt-3 pb-3">
-              <div class="col">
-                <div class="ig-thumbnail">
-                  <img src="img/thumbs/1.png">
+              <?php foreach ($mediaItems as $media): ?>
+                <div class="col-md-3 col-6 mb-4"> <!-- 4 per baris di md ke atas, 2 per baris di xs-sm -->
+                  <div class="ig-thumbnail border rounded overflow-hidden position-relative" style="width: 100%; padding-top: 100%; position: relative;">
+                    <?php if ($media['media_type'] == 'IMAGE' || $media['media_type'] == 'CAROUSEL_ALBUM'): ?>
+                      <img src="<?= htmlspecialchars($media['media_url']); ?>" 
+                          alt="Instagram Media"
+                          style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover;">
+                      <?php if ($media['media_type'] == 'CAROUSEL_ALBUM'): ?>
+                        <div class="carousel-badge position-absolute" style="top: 0.25rem; right: 0.25rem; font-size: 0.7rem; background-color: rgba(0,0,0,0.7); color: white; padding: 0 0.3rem; border-radius: 0.25rem;">
+                          Album
+                        </div>
+                      <?php endif; ?>
+                    <?php elseif ($media['media_type'] == 'VIDEO'): ?>
+                      <video controls 
+                            style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover;">
+                        <source src="<?= htmlspecialchars($media['media_url']); ?>" type="video/mp4">
+                        Your browser does not support the video tag.
+                      </video>
+                    <?php endif; ?>
+                  </div>
                 </div>
-                <div class="ig-thumbnail">
-                  <img src="img/thumbs/2.png">
-                </div>
-                <div class="ig-thumbnail">
-                  <img src="img/thumbs/3.png">
-                </div>
-              </div>
+              <?php endforeach; ?>
             </div>
-
           </div>
         </div>
       </div>
